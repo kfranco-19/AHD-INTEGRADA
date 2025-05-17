3348b0de1fec0ce3898139058eca7f06024b2638
kfranco <44916331+kfranco-19@users.noreply.github.com>
2025-05-16 18:14:39 -0600

    Se actualizo login y el background en todas las paginas

--- js/login.js
+++ js/login.js
@@ -1,11 +1,11 @@
 document.getElementById('loginForm').addEventListener('submit', function(e){
     e.preventDefault();//Evitar que el formulario se envíe automáticamente al servidor.
 
-    const usuario = document.getElementById('user').value;
-    const pass = document.getElementById('password').value;
+    const usuario = document.getElementById('usuario').value;
+    const pass = document.getElementById('pass').value;
 
     //Simulación básica de Login
-    if(user === 'admin' && password === '12345'){
+    if(usuario === 'admin' && pass === '12345'){
         window.location.href = '../private/dashboard.html';
     }
     else{
